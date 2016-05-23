b44f2d17357258037fbdd9933e8f75b800751e47
yshayy <yshayy@gmail.com>
2016-05-23 08:56:54 +0300

    add some tooltips, cleaned a bit

--- presentation/helpers/inject-op-tooltips.js
+++ presentation/helpers/inject-op-tooltips.js
@@ -15,7 +15,7 @@ const createToolTip = (id) => {
 };
 
 const rxImages =
-["map", "scan", "filter", "switch", "distinctUntilChanged", "flatMap"].reduce((acc, op) => ({[op]: `http://reactivex.io/documentation/operators/images/${op}.png`, ...acc } ), {});
+["map", "debounce", "merge", "combineLatest", "scan", "filter", "switch", "distinctUntilChanged", "flatMap"].reduce((acc, op) => ({[op]: `http://reactivex.io/documentation/operators/images/${op}.png`, ...acc } ), {});
 
 Rx.Observable.fromEvent(document.body, "mouseover")
              .distinctUntilChanged(e => e.target)
